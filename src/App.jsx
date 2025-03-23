a802e59d46b0a0c01d069210e9243fe4b62839a6
aleksandrabobyleva <aleksandrabobyleva@gmail.com>
2025-03-23 22:09:57 +0100

    hw-18

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Contacts from "./pages/contacts";
+import Basket from "./pages/basket";
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/basket" element={<Basket />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
